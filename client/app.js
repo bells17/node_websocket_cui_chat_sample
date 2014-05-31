2994a3bac9d84ea9484744b62e527fd0aae694f4
hayakawa <hayakawa@trifort.jp>
2014-05-31 11:47:40 +0900

    complete

---
+++ client/app.js
@@ -0,0 +1,74 @@
+var util = require('util');
+var socket = require('socket.io-client');
+var socket = new socket.connect('http://localhost:3000');
+var shell = require('shell');
+var shell_app = new shell( { chdir: __dirname } )
+var room; 
+//シェルの環境設定
+shell_app.configure(function() {
+	shell_app.use(shell.history({
+		shell: shell_app
+	}));
+	shell_app.use(shell.completer({
+		shell: shell_app
+	}));
+	shell_app.use(shell.router({
+		shell: shell_app
+	}));
+	shell_app.use(shell.help({
+		shell: shell_app,
+		introduction: false
+	}));
+});
+
+//コマンド定義
+shell_app.cmd('send :msg', 'message send', function(req, res, next) {
+	var msg = req.params.msg;
+	socket.emit('send', {'msg':msg});
+	res.prompt();
+});
+shell_app.cmd('rooms', 'get room list', function(req, res, next) {
+	socket.emit('rooms');
+	res.prompt();
+});
+shell_app.cmd('join :name', 'join :name room', function(req, res, next) {
+	var name = req.params.name;
+	socket.emit('join', {'room':name});
+	res.prompt();
+});
+shell_app.cmd('leave', 'leave join room', function(req, res, next) {
+	var name = req.params.name;
+	socket.emit('leave', {'room':room});
+	res.prompt();
+});
+
+//socket.io
+socket.on('connect', function(){
+	console.log('conneted!');
+
+	socket.on('receive', function(data) {
+		console.log("\n"+'received:'+data.msg);
+		shell_app.prompt();
+	});
+
+	socket.on('room_list', function(data) {
+		console.log("\n"+'room_list:'+util.inspect(data));
+		shell_app.prompt();
+	});
+
+	socket.on('joined', function(data) {
+		console.log("\n"+'joined:'+util.inspect(data));
+		room = data.joined_room;
+		shell_app.prompt();
+	});
+
+	socket.on('leaved', function(data) {
+		console.log("\n"+'leaved:'+util.inspect(data));
+		shell_app.prompt();
+	});
+
+	socket.on('disconnect', function(data){
+		console.log('disconnected!');
+	});
+});
+
---
+++ server/app.js
@@ -0,0 +1,75 @@
+var port = 3000;
+var util = require('util');
+var io = require('socket.io').listen(port);
+var _ = require('underscore');
+
+//コネクションが確立した時
+io.sockets.on("connection", function (socket) {
+	console.log("connected");
+	/**
+	 * 各種イベントの登録
+	 */
+	//メッセージ送信
+	socket.on("send", function (data) {
+		console.log("send: " + util.inspect(data));
+		//全員に送信
+		// io.sockets.emit("receive", data);
+		//メッセージ送信者以外に送る
+		// socket.broadcast.to().emit("receive", data);
+		//同じルーム内にいるユーザー全員に対してメッセージを送る
+		var room = get_first_joined_room(socket.store.id, socket.manager.rooms);
+		if (!room) return;
+		console.log('test:'+util.inspect(room));
+		io.sockets.in(room).emit("receive", data);
+	});
+	//ルーム一覧取得
+	socket.on("rooms", function (data) {
+		//送信者1人に対して送信
+		io.sockets.socket(socket.id).emit('room_list', get_rooms(io.rooms));
+	});
+	//ルームに入室
+	socket.on("join", function (data) {
+		if (data.room in rooms) {
+			socket.join(data.room);
+			console.log('test '+util.inspect(socket.manager));
+			io.sockets.socket(socket.id).emit('joined', {response_code:0, joined_room:data.room});
+		} else {
+			io.sockets.socket(socket.id).emit('joined', {response_code:1, joined_room:'failed'});
+		}
+	});
+	//ルームから退出
+	socket.on("leave", function (data) {
+		socket.leave(data.room);
+		io.sockets.socket(socket.id).emit('leaved', {response_code:0, msg:'success'});
+	});
+	/**
+	 * コネクション切断時のイベント
+	 */
+	socket.on("disconnect", function () {
+		io.sockets.emit("message", {value:"user disconnected"});
+	});
+});
+
+
+//args1:io.rooms
+function get_rooms(rooms) {
+	var ret = {};
+	for (key in rooms) {
+  	ret[key.slice(1)] = rooms[key].length;
+	}
+	delete ret[''];
+	return ret;
+}
+
+//roomsからsocket_idが入室している(hashの)一番はじめの部屋を返します
+//args1:socket.store.id
+//args2:socket.manager.rooms
+function get_first_joined_room(socket_id, rooms) {
+	for (var key in rooms) {
+		if (key == '') continue;
+		if (rooms[key].indexOf(socket_id) >= 0) {
+			return key.slice(1);
+		}
+	}
+	return false;
+}
